b46ae007d30013bbfceb3766d30d9f20e0368a91
Aryan Sharma <agsharma98@gmail.com>
2024-11-22 08:33:46 -0800

    Initial Commit

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Aryan Sharma on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
